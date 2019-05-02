283c319afccb67754be5371d53e3ee9d13e4f6ea
Patrick Kim <pkim050@gmail.com>
2019-05-02 11:30:41 +0000

    Automatically backed up by Learn

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,6 +1,6 @@
 class CreateRides < ActiveRecord::Migration
   def change
-    create_table :rides do |t|
+    create_join_table :rides do |t|
       t.integer :passenger_id
       t.integer :taxi_id
       t.timestamps null: false
